d44ac9739f38a6c3ec2d02cf8098fa32749bfb36
Tymoteusz Bak <timi@timiportfolio.com>
2023-10-05 10:10:43 +0200

    add flight type selector

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script setup>
+  import { ref } from 'vue';
+  const flightType = ref('one-way flight');
   
+  function logValue() {
+    console.log('flightType: ', flightType.value);
+  }
+
+  logValue();
 </script>
 
 <template>
-  
+  <select v-model="flightType" @change="logValue">
+    <option value="one-way flight">One-way flight</option>
+    <option value="return flight">Return flight</option>
+  </select>
 </template>
 
 <style>
