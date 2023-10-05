8959b5644989ea913d6044146fd4262bfd519798
Tymoteusz Bak <timi@timiportfolio.com>
2023-10-05 10:44:52 +0200

    fill departure date with today date and return date with current departure date value

--- src/components/FlightBooker.vue
+++ src/components/FlightBooker.vue
@@ -2,11 +2,27 @@
 
 import { ref } from 'vue';
 const flightType = ref('one-way flight');
-const departureDate = ref();
-const returnDate = ref();
+const departureDate = ref(dateToString(new Date()));
+const returnDate = ref(departureDate.value);
+
+function dateToString(date) {
+  return (
+    date.getFullYear() +
+    '-' +
+    pad(date.getMonth() + 1) +
+    '-' +
+    pad(date.getDate())
+  )
+}
+
+function pad(n, s = String(n)) {
+  return s.length < 2 ? `0${s}` : s
+}
 
 function book() {
-    console.log('You are booking ', flightType.value);
+    console.log('You are booking', flightType.value);
+    console.log('Your departure date: ', departureDate.value);
+    console.log('Your return date: ', returnDate.value);
 }
 </script>
 
